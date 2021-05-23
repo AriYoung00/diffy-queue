f60c90b575cb97303edd6c2f20e50c77b48fb120
Ariel Young <arielyoung103@gmail.com>
2021-05-23 15:47:25 -0700

    Added Item to solved trait

--- src/solver.rs
+++ src/solver.rs
@@ -1,4 +1,4 @@
-pub trait Solver: Iterator {
+pub trait Solver: Iterator<Item = (f32, f32)> {
     fn solve_at_point(&mut self, t: f32) -> Option<f32>;
     fn _next(&mut self) -> Option<(f32, f32)>;
 }
