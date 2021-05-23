883f45370120a901e09cc66f0664854047352bdb
Ariel Young <arielyoung103@gmail.com>
2021-05-23 15:46:26 -0700

    Tweaked graph settings

--- examples/graph_euler_rk4_img.rs
+++ examples/graph_euler_rk4_img.rs
@@ -11,7 +11,7 @@ fn graph_series_in_bounds(x_min: i32, x_max: i32, y_min: i32, y_max: i32) {
 
     root.fill(&WHITE).unwrap();
     let mut ctx = ChartBuilder::on(&root)
-        .caption("Test Figure", ("Arial", 30))
+        .caption("dx/dt = t*x, h=0.01", ("Arial", 30))
         .margin(10)
         .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
@@ -21,14 +21,14 @@ fn graph_series_in_bounds(x_min: i32, x_max: i32, y_min: i32, y_max: i32) {
     ctx.configure_mesh().draw().unwrap();
 
     let f_t_x = |t, x| t*x;
-    let mut solver = euler::create_euler_solver(f_t_x, 0.0, 1.0, 0.01);
-    let mut rk4_solver = rk::create_rk4_solver(f_t_x, 0.0, 1.0, 0.01);
+    let mut solver = euler::create_euler_solver(&f_t_x, 0.0, 1.0, 0.01);
+    let mut rk4_solver = rk::create_rk4_solver(&f_t_x, 0.0, 1.0, 0.01);
     let actual_soln = |t: f32| E.powf(0.5 * t.powf(2.0));
 
     println!("computed(1.0) = {}", solver.solve_at_point(1.0).unwrap());
 
     ctx.draw_series(
-        LineSeries::new((x_min*10..x_max*10)
+        LineSeries::new((x_min*10..=x_max*10)
                             .map(|x| x as f32 / 10.0)
                             .map(|x| (x, solver.solve_at_point(x).unwrap())), &RED)
     ).unwrap()
@@ -58,6 +58,6 @@ fn graph_series_in_bounds(x_min: i32, x_max: i32, y_min: i32, y_max: i32) {
 }
 
 fn main() {
-    graph_series_in_bounds(2, 3, 0, 80);
+    graph_series_in_bounds(0, 3, 0, 80);
 }
 
